1cd900ee3e2e4317949a4f5dd6400360c542873e
Viktor Soshko <Lorgar4@ukr.net>
2020-02-09 11:47:42 +0300

    Добавлено отображение приоритета задачи

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -7,8 +7,26 @@ import Dropdown from '../dropdown';
 import './card.scss';
 
 
+const Priority = ({priority}) => {
+    const text = {low: 'Низкая', normal: 'Средняя', high: 'Высокая'};
+    const color = {low: 'green', normal: 'orange', high: 'red'};
+
+    return (
+        <span style={{fontSize: '0.8rem'}}>
+            Важность:<br/>
+            <span 
+                style={{color: color[priority], fontSize: '1.2rem'}}
+            >
+                {text[priority]}
+            </span>
+        </span>
+    );
+    
+}
+
+
 const Card = ( {task, taskDone } ) => {
-    const {done, imgSrc, title, text, id, open} = task;
+    const {done, imgSrc, title, text, id, open, priority} = task;
     return (
         <div className="card">
             {done && <CheckBox onChange={() => taskDone(id)}/>}
@@ -20,6 +38,7 @@ const Card = ( {task, taskDone } ) => {
                 <p>{ text }</p>
             </div>
             <div className="card-action">
+                <Priority priority={priority}/>
                 {!done && <Dropdown open={open} id={id} />}
             </div>
         </div>
