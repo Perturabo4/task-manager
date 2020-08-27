5156343565cdaf60ce4928e67c9b5f481595eadc
Perturabo4 <Lorgar4@ukr.net>
2020-08-27 10:42:38 +0300

    Реализована дополнительная обработка возможных ошибок при авторизации/регистрации пользователя

--- src/reducers/auth_reducer.js
+++ src/reducers/auth_reducer.js
@@ -7,10 +7,21 @@ const initialState = {
     }
 
 const authErrorHandler = (errorText) => {
-    switch(errorText) {
+    console.log(errorText);
+    let text = errorText.includes(' ') 
+                ? errorText.slice(0, errorText.indexOf(' '))
+                : errorText;
+    switch(text) {
         case 'INVALID_EMAIL':
         case 'EMAIL_NOT_FOUND':
+        case 'INVALID_PASSWORD':
             return 'Неверный логин или пароль';
+        case 'WEAK_PASSWORD':
+            return 'Пароль должен содержать не менее 6 символов';
+        case 'EMAIL_EXISTS':
+            return 'Пользователь с таким емейлом уже существует';
+        case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+            return 'Слишком много неудачных попыток ввода. Попробуйте позже.'
         default:
             return 'Возникла ошибка при авторизации. Попробуйте повторить попытку позже.';
     }
