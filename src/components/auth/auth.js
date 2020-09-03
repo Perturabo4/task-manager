4936ec8b047f401da83699c00e50c8203f52d817
Perturabo4 <Lorgar4@ukr.net>
2020-09-03 09:05:37 +0300

    Изменен заголовок для страницы регистрации

--- src/components/auth/auth.js
+++ src/components/auth/auth.js
@@ -1,78 +1,82 @@
-import React from 'react';
-import {Redirect} from 'react-router-dom';
-import {connect} from 'react-redux';
-import WithAuthService from '../hoc/with-auth-service';
-import {compose} from '../../utils';
-import {setAuthEmail, setAuthPass, authUser} from '../../actions';
-import Button from '../button';
-import AuthError from '../auth-error';
+import React from "react";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import WithAuthService from "../hoc/with-auth-service";
+import { compose } from "../../utils";
+import { setAuthEmail, setAuthPass, authUser } from "../../actions";
+import Button from "../button";
+import AuthError from "../auth-error";
 
-import './auth.scss';
+import "./auth.scss";
 
-const Auth = ({email, pass, userId, setAuthEmail, setAuthPass, authUser, authError}) => {
-
-    if(userId) { 
-        return <Redirect to={"/app"}/>
-    }
-    const buttonActive = !email || !pass ? 'disabled' : '';
-    return (
+const Auth = ({
+  email,
+  pass,
+  userId,
+  setAuthEmail,
+  setAuthPass,
+  authUser,
+  authError,
+}) => {
+  if (userId) {
+    return <Redirect to={"/app"} />;
+  }
+  const buttonActive = !email || !pass ? "disabled" : "";
+  return (
     <div className="auth-background">
-        <h3>Войдите в систему или зарегист&shy;рируйтесь</h3>
-        <form 
-            className="auth-form"
-            onSubmit={(e) => e.preventDefault()}
-        >
-            <div className="input-field">
-                <input 
-                    id="email"
-                    type="email"
-                    className="validate"
-                    value={email}
-                    onChange={(e) => setAuthEmail(e.target.value)}
-                />
-                <label htmlFor="email">Email</label>
-                <span className="helper-text" data-error="Неверный e-mail"></span>
-            </div>
-            <div className="input-field">
-                <input 
-                    id="password"
-                    type="password"
-                    className="validate"
-                    onChange={(e) => setAuthPass(e.target.value)}
-                />
-                <label htmlFor="password">Пароль</label>
-                <span className="helper-text" data-error="wrong"></span>
-            </div>
-            
-            {
-                authError && <AuthError text={authError}/>
-            }
-            <Button
-                cls={['red', buttonActive]}
-                text={"Войти"}
-                onClick={() => authUser(email, pass)}  
-            />
-            <Button 
-                cls={['red', buttonActive]}
-                text={"Зарегистрироваться"} 
-                onClick={() => authUser(email, pass, true)}  
-            />
-        </form>
+      <h3>
+        Введите логин и пароль для входа или регистрации нового пользователя
+      </h3>
+      <form className="auth-form" onSubmit={(e) => e.preventDefault()}>
+        <div className="input-field">
+          <input
+            id="email"
+            type="email"
+            className="validate"
+            value={email}
+            onChange={(e) => setAuthEmail(e.target.value)}
+          />
+          <label htmlFor="email">Email</label>
+          <span className="helper-text" data-error="Неверный e-mail"></span>
+        </div>
+        <div className="input-field">
+          <input
+            id="password"
+            type="password"
+            className="validate"
+            onChange={(e) => setAuthPass(e.target.value)}
+          />
+          <label htmlFor="password">Пароль</label>
+          <span className="helper-text" data-error="wrong"></span>
+        </div>
+
+        {authError && <AuthError text={authError} />}
+        <Button
+          cls={["red", buttonActive]}
+          text={"Войти"}
+          onClick={() => authUser(email, pass)}
+        />
+        <Button
+          cls={["red", buttonActive]}
+          text={"Зарегистрироваться"}
+          onClick={() => authUser(email, pass, true)}
+        />
+      </form>
     </div>
-    )
-}
+  );
+};
 
-const mapStateToProps = ({auth}) => auth;
+const mapStateToProps = ({ auth }) => auth;
 
-const mapDispatchToProps = (dispatch, {AuthService}) => {
-      return {
-        setAuthEmail: (email) => dispatch(setAuthEmail(email)),
-        setAuthPass: (pass) => dispatch(setAuthPass(pass)),
-        authUser: authUser(dispatch, AuthService)
-      }
-}
+const mapDispatchToProps = (dispatch, { AuthService }) => {
+  return {
+    setAuthEmail: (email) => dispatch(setAuthEmail(email)),
+    setAuthPass: (pass) => dispatch(setAuthPass(pass)),
+    authUser: authUser(dispatch, AuthService),
+  };
+};
 
 export default compose(
-    WithAuthService(),
-    connect(mapStateToProps, mapDispatchToProps)
-)(Auth); 
+  WithAuthService(),
+  connect(mapStateToProps, mapDispatchToProps)
+)(Auth);
