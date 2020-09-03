aeaec5feea67171e77eef8776e4df419ba29b398
Perturabo4 <Lorgar4@ukr.net>
2020-09-03 14:11:46 +0300

    Url вынесены в константы

--- src/services/task-service.js
+++ src/services/task-service.js
@@ -1,55 +1,52 @@
+import API_URL from "../constants";
+
 export default class TaskService {
+  constructor() {
+    this.apiUrl = API_URL;
+  }
 
-    constructor() {
-        this.apiUrl = 'https://task-manager-55ca3.firebaseio.com/'
+  getTasks(userId) {
+    return fetch(`${this.apiUrl} ${userId}/tasks.json`)
+      .then((response) => response.json())
+      .then((data) => {
+        return Object.keys(data).map((key) => ({ ...data[key], id: key }));
+      })
+      .catch((err) => new Error("Something went wrong"));
+  }
+
+  createTask(task, userId) {
+    return fetch(`${this.apiUrl} ${userId}/tasks.json`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+  }
+
+  updateTask(id, task, singleKey, userId) {
+    if (singleKey) {
+      const [key, val] = singleKey;
+
+      return fetch(`${this.apiUrl} ${userId}/tasks/${id}/${key}.json`, {
+        method: "PUT",
+        body: val,
+      });
     }
 
-    getTasks(userId) {
-        return fetch(`${this.apiUrl} ${userId}/tasks.json`)
-                .then( response => response.json())
-                .then( data => {
-                    return  Object.keys(data).map( key => ({...data[key], id: key}));
-                })
-                .catch( err => new Error('Something went wrong'))
-    }
+    return fetch(`${this.apiUrl} ${userId}/tasks/${id}.json`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
+  }
 
-    createTask(task, userId) {
-       return fetch(`${this.apiUrl} ${userId}/tasks.json`, {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(task)
-        })
-    }
-
-    updateTask(id, task, singleKey, userId) {
-
-        if(singleKey) {
-
-            const [key, val] = singleKey;
-
-            return fetch(`${this.apiUrl} ${userId}/tasks/${id}/${key}.json`, {
-                method: 'PUT',
-                body: val
-            })
-        }
-
-        return fetch(`${this.apiUrl} ${userId}/tasks/${id}.json`, {
-            method: 'PUT',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(task)
-         })
-     
-     }
-
-    deleteTask(id, userId) {
-        console.log(`${this.apiUrl} ${userId}/tasks/${id}.json`);
-        return fetch(`${this.apiUrl} ${userId}/tasks/${id}.json`, {
-             method: 'DELETE'
-         })
-     
-     }
+  deleteTask(id, userId) {
+    console.log(`${this.apiUrl} ${userId}/tasks/${id}.json`);
+    return fetch(`${this.apiUrl} ${userId}/tasks/${id}.json`, {
+      method: "DELETE",
+    });
+  }
 }
